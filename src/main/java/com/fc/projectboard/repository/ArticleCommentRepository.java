a366ed99a9ed9be2015dbc4262c0e6599cb9ac53
Pooky112 <97931167+Pooky112@users.noreply.github.com>
2023-02-01 20:31:18 +0900

    #10 - repository interface 작성
    db에 접근 위해

---
+++ src/main/java/com/fc/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,8 @@
+package com.fc.projectboard.repository;
+
+import com.fc.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
+}
